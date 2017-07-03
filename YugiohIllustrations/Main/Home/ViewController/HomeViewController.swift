e04d763eda5a417f0406426f38f52a5061c390fe
Shvier <daulvi@hotmail.com>
2017-07-03 21:46:27 +0800

    Save

--- YugiohIllustrations/Main/Home/ViewController/HomeViewController.swift
+++ YugiohIllustrations/Main/Home/ViewController/HomeViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    func leftBarItemAction(sender: UIBarButtonItem) {
+        let settingVC = SHSettingViewController()
+        present(settingVC, animated: true, completion: nil)
+    }
+    
+    func initUI() {
+        configNavi()
+    }
+    
+    func configNavi() {
+        let leftBarItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(self.leftBarItemAction(sender:)))
+        navigationItem.leftBarButtonItem = leftBarItem
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ YugiohIllustrations/Main/Setting/ViewController/SHSettingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SHSettingViewController.swift
+//  YugiohIllustrations
+//
+//  Created by shvier on 03/07/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class SHSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
